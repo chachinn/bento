8014f8303b6dd939b08b32b4f719bd0ed180088e
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-22 20:27:14 +0800

    Recognize catfish in shared allergen inference

--- data/recipe-quality-runtime.js
+++ data/recipe-quality-runtime.js
@@ -14,7 +14,7 @@ function inferAllergens(ingredients){
     .replace(/\b(coconut|soy|almond|oat|rice|cashew|macadamia) milk\b/g,' plantmilk ')
     .replace(/\b(peanut|almond|cashew|walnut|hazelnut|pistachio|macadamia|sunflower seed) butter\b/g,' nutbutter ');
   if(has(dairy,/\b(whole milk|fresh milk|evaporated milk|condensed milk|buttermilk|cream|heavy cream|whipping cream|crème fraîche|butter|ghee|cheese|parmesan|mozzarella|cheddar|beaufort|comté|emmental|gruyère|reblochon|fromage blanc|tomme|cantal|paneer|chhena|chhenna|chenna|chhurpi|khoya|khoa|mawa|rabri|curd|dahi|yogurt|yoghurt|milk powder|powdered milk|ice cream)\b/)||/(^|[^a-z])milk([^a-z]|$)/.test(dairy))out.push('milk');
-  if(has(raw,/\b(fish|fish sauce|bonito|katsuobushi|dashi|anchov(?:y|ies)|salmon|tuna|mackerel|cod|sardines?|fish cake|chikuwa|kamaboko|tilapia|bangus|milkfish|sea bass|sea bream|bream|monkfish|hake|dogfish|swordfish|marlin|rockfish|whitebait|whiting|eel|perch|trout|snapper|sole|zander|lamprey|shirasu|hilsa|ilish|rohu|carp|fermented fish|fermented dried fish)\b/))out.push('fish');
+  if(has(raw,/\b(fish|fish sauce|bonito|katsuobushi|dashi|anchov(?:y|ies)|salmon|tuna|mackerel|cod|sardines?|fish cake|chikuwa|kamaboko|tilapia|catfish|bangus|milkfish|sea bass|sea bream|bream|monkfish|hake|dogfish|swordfish|marlin|rockfish|whitebait|whiting|eel|perch|trout|snapper|sole|zander|lamprey|shirasu|hilsa|ilish|rohu|carp|fermented fish|fermented dried fish)\b/))out.push('fish');
   const shell=raw.replace(/\b(vegetarian|vegan|mushroom) oyster sauce\b/g,' oyster-style-sauce ');
   if(has(shell,/\b(shrimp|prawns?|crabs?|clams?|mussels?|oyster(?:s| sauce)?|scallops?|squid|octopus|abalone|lobster|langoustines?|cuttlefish|limpets?|snails?|cockles?|razor clams?)\b/))out.push('shellfish');
   if(has(raw,/\b(peanuts?|peanut butter|cashews?|walnuts?|almonds?|hazelnuts?|pistachios?|pine nuts?|chestnuts?|pecans?|macadamia)\b/))out.push('nuts');
@@ -33,8 +33,8 @@ for(const r of lib){
     r.photoQueries=[`${r.title} ${r.cuisine||''} food`.trim(),`${r.title} dish`];
     photos[r.id]={queries:r.photoQueries,square:true,source:'Wikimedia Commons runtime search'};
   }
-  r.recipeQualityVersion=35;
+  r.recipeQualityVersion=36;
 }
-window.BENTO_RECIPE_QUALITY_VERSION=35;
+window.BENTO_RECIPE_QUALITY_VERSION=36;
 window.BENTO_INFER_ALLERGENS=inferAllergens;
 })();
